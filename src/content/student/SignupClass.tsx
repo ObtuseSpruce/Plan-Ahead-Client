19a19a6480be0cabc83fa3e3a59e4a4e848eef29
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-21 17:05:55 -0700

    Signup for a class form by student almost ready

--- src/content/student/SignupClass.tsx
+++ src/content/student/SignupClass.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import {Redirect} from 'react-router-dom'
+
 
 interface PropsInt {
     user: {
@@ -10,22 +12,39 @@ interface PropsInt {
     }
   }
 
- export  interface AllClasses{
-     _id: string,
-     classname: string
- }
+  interface ClassModel {
+    select?: boolean; 
+    _id: Object; 
+    classname:  string;
+    subject:    string;
+    teacher:   string;
+    students?:   Array<string>;
+    assignments?: Array<string>;
+    startdate:  Date;
+    enddate:    Date;
+}
+
 
 const SignupClass : React.FC<PropsInt> = (props) => {
-
-   let [classes, setClasses] = useState([]) 
+    let [classes, setClasses] = useState<ClassModel[]>([])
+    let [selectedClasses, setSelectedClasses]= useState<ClassModel[]>([])
+    let [message, setMessage] = useState('')
 
     useEffect(() => {
-        console.log('Hello!')
         callApi()
       }, [])
 
-    // Function to call the API and retrieve the classes
-    const callApi =()=>{
+      if(!props.user) {
+        return <Redirect to='/login'/>
+      }
+      
+      let userStr = props.user.position.toLowerCase() 
+      if(userStr == "teacher"){
+        return <Redirect to='/profile'/>
+      }
+
+      // Function to call the API and retrieve the classes
+     const callApi =()=>{
         fetch(process.env.REACT_APP_SERVER_URL + 'classes')
         .then(response=> response.json())
         .then(data=>{
@@ -37,19 +56,98 @@ const SignupClass : React.FC<PropsInt> = (props) => {
         })
     }
 
-    let allClasses = classes.map((cl:any, i:number)=>{
+    let allClasses =  classes.map((cl, i)=>{
         return (
-          
-            <div> {cl.classname} </div>
-        )
-              
+                <tr key= {i}>
+                    <th>
+                            <input type="checkbox" 
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>)=>{
+                            let checked = event.target.checked
+                            let classesWithSelect = classes.map((c,i)=>{
+                                    if(cl._id === c._id){
+                                        c.select = checked 
+                                        console.log("c.select",c.select)
+                                        console.log("c",c)
+                                    }
+                                    return c
+                                }).filter((c)=>{
+                                    return c.select
+                                })
+                                setSelectedClasses(classesWithSelect)
+                        }} 
+                        checked={cl.select}></input>
+                    </th> 
+                    <td>{cl.classname} </td>
+                    <td>{cl.subject}</td>
+                    <td>{cl.startdate}</td>
+                    <td>{cl.teacher}</td>
+                    <td>{cl.enddate}</td>
+                </tr>
+        )    
     })
+  
+    const handleSubmit = (e: React.FormEvent) => {
+
+        e.preventDefault()
+        let  classIdSelected :Array<string>  = []
+          classIdSelected = selectedClasses.map((cl,i)=>{
+             return cl._id.toString()
+        })
+        console.log("class id selected",classIdSelected)
+      
+        fetch(process.env.REACT_APP_SERVER_URL + '', {
+            method: 'POST',
+            body: JSON.stringify({
+                classIdSelected
+            }),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+          }
+        )
+        .then(response => {
+            console.log("Here is the response!", response)
+            if (!response.ok){
+              setMessage(`${response.status} : ${response.statusText}`)
+              return
+            }
+            response.json().then(result => {
+              console.log("result!", result)
+            })
+        })
+        .catch(err => {
+            console.log('error in signup class submit', err)
+        })
+        
+
+    }
 
    return(
           <div>
                 <h2>Sign Up for Class</h2>
-                <div>{allClasses}</div>
+                <span className="red">{message}</span>
 
+                <div className="signUpTable">
+                    <form onSubmit={handleSubmit}>
+                          
+                        <button type="submit">Register for Classes</button>
+                    </form> 
+                        <table>
+                                <thead>
+                                    <tr>
+                                        <th>Select</th>
+                                        <th>ClassName</th>
+                                        <th>Subject</th>
+                                        <th> Start Date</th>
+                                        <th>Teacher</th>
+                                        <th>End Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody> 
+                                    {allClasses}
+                                </tbody>
+                        </table>
+                </div>
           </div>
           )
     }
--- src/content/student/StudentHome.tsx
+++ src/content/student/StudentHome.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
-import SignupClass from './SignupClass'
+
 
 interface PropsInt {
   user: {
