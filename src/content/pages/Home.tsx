5cc68a0b1357080cfaaa14d6ab3ea6311dce7eac
ObtuseSpruce <williamllapitan@gmail.com>
2020-05-24 10:11:03 -0700

    gave the logo undertext

--- src/content/pages/Home.tsx
+++ src/content/pages/Home.tsx
@@ -9,9 +9,10 @@ const Home : React.FC<{}> = () =>{
   return (
     <ThemeProvider theme={FrontTheme}>
       <div className="logoFont">
-        <h1>
-          Plan-Ahead
-        </h1>
+        <h1>Plan-Ahead</h1>
+      </div>
+      <div className="bottomLogo">
+        <p>~your academic success now~</p>
       </div>
     </ThemeProvider>
   )
