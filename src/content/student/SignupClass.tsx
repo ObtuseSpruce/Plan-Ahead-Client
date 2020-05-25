627158112435fe820e68e7d8e7c150062b532a16
ObtuseSpruce <williamllapitan@gmail.com>
2020-05-25 14:14:35 -0700

    merged

--- src/content/student/SignupClass.tsx
+++ src/content/student/SignupClass.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import {Redirect} from 'react-router-dom'
--- src/content/student/ViewSignedUpClasses.tsx
+++ src/content/student/ViewSignedUpClasses.tsx
@@ -3,10 +3,6 @@ import {Redirect} from 'react-router-dom'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Button from '@material-ui/core/Button';
 
 interface PropsInt {
     user: {
