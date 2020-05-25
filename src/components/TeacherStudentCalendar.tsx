0e276514bfaf3dcbf1296fb93a0ec7398b9890e4
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-25 14:42:06 -0700

    Removed extra line from calendar

--- src/components/TeacherStudentCalendar.tsx
+++ src/components/TeacherStudentCalendar.tsx
@@ -13,7 +13,6 @@ import { blue } from '@material-ui/core/colors';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Button from '@material-ui/core/Button'
-
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
